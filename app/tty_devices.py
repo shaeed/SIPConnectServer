387a87994111cd72dc6d6f27a696123a1447221b
Shaeed Khan <shaeed.khan46@gmail.com>
2025-07-26 11:10:41 +0300

    Fixed the bug in tty device search.

--- app/tty_devices.py
+++ app/tty_devices.py
@@ -1,7 +1,4 @@
-
-import re
-
-import asyncio
+import glob
 from typing import List
 
 
@@ -10,16 +7,6 @@ async def read_ttyUSB_devices() -> List[str]:
     Extracts all /dev/ttyUSB* device paths from the input string.
     :returns: ['/dev/ttyUSB0', '/dev/ttyUSB1', '/dev/ttyUSB2']
     """
-    process = await asyncio.create_subprocess_exec(
-        'ls', '-l', '/dev/ttyUSB*',
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    stdout = stdout.decode()
-
-    pattern = r'/dev/ttyUSB\d+'
-    devices = re.findall(pattern, stdout)
-    print(stdout)
-    print("Devices:", devices)
-    return devices
+    files = glob.glob('/dev/ttyUSB*')
+    print("Devices:", files)
+    return files
